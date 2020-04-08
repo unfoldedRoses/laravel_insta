61327721b04f86af6120001b9182eb3248361d88
praveen <ptest5557@gmail.com>
2020-04-09 01:55:19 +0530

    First Commit

---
+++ fresh/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class ProfileController extends Controller
+{
+    public function index($user){
+
+       $user=User::find($user);
+       
+        return view('home',[
+            "user"=>$user,
+        ]);
+    }
+}
---
+++ fresh/resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <DIV class="ROW">
+
+    <div class="COL-3 p-0">
+<img src="svg/DASH1.svg" alt="">
+</div>
+<div class="COL-9 p-5">
+    <h1>social media making in laravel</h1>
+    <div class="d-flex">
+        <div class="pr-3"><strong>1k</strong> followers</div>
+        <div class="pr-3"><strong>190k</strong>posts</div>
+        <div class="pr-3"><strong>11k</strong>shares</div>
+        
+    </div>
+    <div class="pt-4 font-weight-bold">laravel is very powerfull</div>
+    <div class="pt-4 font-weight-bold">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus, vitae quisquam laudantium repellat amet, dolorem incidunt quaerat commodi id dolores fugit sint? Cum numquam praesentium quaerat natus, quae iusto mollitia maiores eligendi laudantium itaque, rem inventore aspernatur libero odio voluptatum tempora ducimus minima voluptatibus quasi assumenda nemo veritatis pariatur! Quasi dolorum, voluptate asperiores earum aperiam atque illum totam neque cumque sapiente aspernatur soluta maiores, odit sed possimus! At culpa impedit provident adipisci dolor ipsa minima esse sequi. Beatae minima modi voluptate cumque, aperiam ipsum iure a labore ea maiores accusantium voluptatem. Iusto quod dicta quisquam 
+        dolorem molestias voluptatem alias esse!</div>
+    <a href="#">Praveen.com</div>
+
+</div>
+        </DIV>
+<div class="row d-flex">
+    <div class="col-4 pl-5"><img src="https://picsum.photos/200/300" alt="" class="w-100" height="100" width="200"></div>
+
+    <div class="col-4 "><img src="https://picsum.photos/200/100" alt="" class="w-100" height="100" width="200"></div>
+
+    <div class="col-md-4 "><img src="https://picsum.photos/200/200" alt="" class="w-100" height="100" width="200"></div>
+</div>
+
+
+    </div>
+</div>
+@endsection
